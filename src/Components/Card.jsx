0231822b743cf91d16bf07bfa0f0d3be4598b3eb
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-21 00:30:28 +0530

    card completed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,12 +4,12 @@ const Card = () => {
   return (
     <>
     <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
-         {/* Card */}
-      <div className='rounded-xl relative'>
+ {/* Card */}
+ <div className='rounded-xl relative'>
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-          <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
-          <p className='px-2'>Through 8/26</p>
+          <p className='font-bold text-2xl px-2 pt-4'>Omlet with Bread croumps</p>
+          <p className='px-2'>2 Breads and 2 omlets</p>
           <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
         </div>
         <img
@@ -18,6 +18,45 @@ const Card = () => {
           alt='/'
         />
       </div>
+
+
+
+
+ {/* Card */}
+ <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Omlet with Bread croumps</p>
+          <p className='px-2'>2 Breads and 2 omlets</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          alt='/'
+        />
+      </div>
+
+
+
+
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Omlet with Bread croumps</p>
+          <p className='px-2'>2 Breads and 2 omlets</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          alt='/'
+        />
+      </div>
+
+
+
     </div>
     
     </>
