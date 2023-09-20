f43cc4b8319161a59c4ed3a2bb7a4ea6453555c2
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-21 01:07:54 +0530

    Food card data  fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Card from './Components/Card'
+import FoodCard from './Components/FoodCard'
 import Home from './Components/Home'
 import Navbar from './Components/Navbar'
 
@@ -12,6 +13,8 @@ function App() {
     <Navbar/>
     <Home/>
     <Card/>
+    <FoodCard/>
+
 
 
     </>
---
+++ src/Components/FoodCard.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import {data} from '../data/data.js'
+const FoodCard = () => {
+    const [foods , setFoods] = useState(data)
+  return (
+    <div className='max-w-[1640px] m-auto px-4 py-12'>
+        <div className='flex justify-center items-center'>
+        <h1 className='text-4xl font-bold '>Top Food Items to Try Now!</h1>
+        </div>
+
+        <div className='FILTER ROW flex flex-col lg:flex-row justify-between'>
+            <div>
+                <p>Filter type</p>
+                <div className='flex justify-start flex-wrap gap-3'>
+                <button>All</button>
+                <button>Burgers</button>
+                <button>Pizza</button>
+                <button>Salads</button>
+                <button>Chicken</button>
+                </div>
+
+            </div>
+
+            <div >
+                <p>Filter price</p>
+                <div className='flex justify-start flex-wrap gap-3'>
+                <button>$</button>
+                <button>$$</button>
+                <button>$$$</button>
+                <button>$$$$</button>
+
+                </div>
+
+            </div>
+        </div>
+
+
+        {/* DISPLAY FOODS */}
+
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+{
+    foods.map((food, index)=>(
+        <div key={index} className='shadow-xl rounded-xl border'>
+            <img className='w-full h-[200px] object-cover rounded-t-xl' src={food.image} alt="" />
+            <div className='flex justify-between py-4 px-2'>
+                <p>{food.name}</p>
+                <p>{food.price}</p>
+
+            </div>
+        </div>
+
+    ))
+}
+        </div>
+    </div>
+  )
+}
+
+export default FoodCard
